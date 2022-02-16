163c662eccde924db23a87b39a69277984b62c46
Jean-Olivier Irisson <irisson@normalesup.org>
2022-02-16 20:40:51 +0100

    Working version of the code to download images
    Either locally from the vault or through the network

---
+++ 2.download_images.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+#
+# Download images from EcoTaxa
+#
+# (c) 2022 Jean-Olivier Irisson, GNU General Public License v3
+
+import os
+import yaml
+
+import shutil
+import urllib.request
+
+from tqdm import tqdm
+from pyarrow.parquet import read_table
+
+# read config
+with open(r'config.yaml') as config_file:
+  cfg = yaml.safe_load(config_file)
+
+# prepare storage
+data_dir = os.path.join(os.path.expanduser(cfg['base_dir']), cfg['dataset'])
+img_dir = os.path.join(data_dir, 'imgs_raw')
+os.makedirs(img_dir, exist_ok=True)
+
+# read data from EcoTaxa
+df = read_table(os.path.join(data_dir, 'orig_extraction.parquet')).to_pandas().reset_index()
+# TODO remove the reset index once it has been fixed in the previous step
+
+# detect image extension
+file,ext = os.path.splitext(df['img.file_name'][0])
+# name image according to internal object_id to ensure uniqueness
+df['dest_path'] = [os.path.join(img_dir, str(this_id)+ext) for this_id in df['objid']]
+
+# download images (with a nice progress bar)
+vault_path = '/remote/ecotaxa/vault'
+for i in tqdm(range(df.shape[0])):
+  # if the file has not been copied already
+  if not os.path.isfile(df['dest_path'][i]):
+    # copy from vault
+    if os.path.isdir(vault_path):
+      res = shutil.copyfile(
+        src=os.path.join(vault_path, df['img.file_name'][i]),
+        dst=df['dest_path'][i]
+      )
+    # or copy through the internet
+    else:
+      res = urllib.request.urlretrieve(
+        url='https://ecotaxa.obs-vlfr.fr/vault/'+df['img.file_name'][i],
+        filename=df['dest_path'][i]
+      )
