e9ade945cb926b7f4777b7f3427f306d689431a9
Jean-Olivier Irisson <irisson@normalesup.org>
2022-02-23 14:58:17 +0000

    First working version of feature extraction

---
+++ 4.process_images_with_skimage.R
@@ -0,0 +1,91 @@
+#!/usr/bin/Rscript
+#
+# Read taxo grouping and split train/val/test
+#
+# (c) 2022 Jean-Olivier Irisson, GNU General Public License v3
+
+library("yaml")
+library("tidyverse", warn.conflicts=FALSE)
+library("arrow")
+library("fs")
+library("parallel")
+
+# read config
+cfg = read_yaml("config.yaml")
+
+# define i/o directory
+data_dir = file.path(cfg$base_dir, cfg$dataset)
+
+message("Define images to process") # ----
+
+# read extracted data and the regrouped taxon name
+df = left_join(
+    read_parquet(file.path(data_dir, "orig_extraction.parquet"), col_select=c("objid", "img.file_name")),
+    read_csv(file.path(data_dir, "taxa.csv.gz"), col_select=c("objid","taxon"), col_types=cols()),
+    by="objid"
+  )
+
+# read any existing features file
+skfeatures_file = file.path(data_dir, 'features_skimage.csv.gz')
+if (file_exists(skfeatures_file)) {
+  features = read_csv(skfeatures_file, col_types=cols())
+} else {
+  features = data.frame() #NB: not tibble, for compatibility below
+}
+
+# extract file extension and compute file paths
+df = mutate(df,
+  ext=tools::file_ext(df$img.file_name),
+  source=file.path(data_dir, "orig_imgs", str_c(objid, ".", ext)),
+  dest=file.path(data_dir, "imgs", taxon, str_c(objid, ".", ext)),
+)
+
+# test for the existence of all original images
+missing_imgs = sum(!file.exists(df$source))
+if (missing_imgs > 0) {
+  stop(missing_imgs, "are missing. Please run ./2.download_images.py again.")
+}
+
+# define which images to process
+df = df %>%
+  filter(
+    # either the processed image is missing
+    ! file.exists(dest) |
+    # or the measured features are missing
+    ! (objid %in% features$objid)
+  )
+# remove the existing features for those to avoid duplicates
+features = filter(features, objid %in% df$objid)
+message("  ", nrow(df), " images to process")
+
+message("Process images") # ----
+
+# make destination directories
+file.path(data_dir, unique(df$taxon)) %>% walk(dir.create, showWarnings=FALSE)
+
+# process images, in parallel
+sk = reticulate::import_from_path("lib_skimage")
+
+# split the data set in chunks to be processed
+chunk = 5000
+n_cores = 40
+n = nrow(df)
+dfl = split(df, rep(1:ceiling(n/chunk),each=chunk)[1:n])
+# and process them in parallel
+props = mclapply(dfl, function(x) {sk$process_images(x, cfg)}, mc.cores=n_cores)
+# then get the info back
+props = do.call(bind_rows, props) %>%
+  mutate(objid=df$objid) %>%
+  select(objid, everything())
+features = bind_rows(features, props) %>%
+  arrange(objid)
+
+message("Save data and cleanup") # ----
+
+# save the features
+write_csv(features, file=skfeatures_file)
+
+# remove extra images
+all_imgs = list.files(file.path(data_dir, "imgs"), full.names=TRUE, recursive=TRUE)
+extra_imgs = all_imgs[!all_imgs %in% df$dest]
+unlink(extra_imgs)
---
+++ lib_skimage.py
@@ -0,0 +1,83 @@
+from skimage import io
+from skimage import measure
+import numpy as np
+import pandas as pd
+
+# Fast way to get the area from an object of type RegionProperties
+def fast_area(x) :
+    return(np.sum(x._label_image[x._slice] == x.label))
+
+# Process one image
+def process_image(source, dest, cfg):
+    # read input image
+    img = io.imread(source)
+    
+    if cfg['images']['grayscale'] and len(img.shape) == 3:
+        img = img[:,:,0]
+    # pdb.set_trace()
+    if cfg['images']['crop'] > 0:
+        height = img.shape[0]
+        img = img[0:(height-cfg['images']['crop']),:]
+        # TODO generalise to cropping all sides Cf ecotaxa_ML_template
+
+    # invert image (makes it easier to write it afterwards)
+    img = 255 - img
+    # TODO make this optional or think about what it means
+
+    # threshold it and detect particles
+    img_thresholded = img > 4
+    img_labelled = measure.label(img_thresholded, background=False, connectivity=2)
+
+    # measure particles and keep only the largest one
+    regions = measure.regionprops(label_image=img_labelled, intensity_image=img)
+    areas = [fast_area(r) for r in regions]
+    particle_label = np.argmax(areas) + 1
+
+    # properties to compute and save
+    prop_names = ['area',
+                  'convex_area',
+                  'eccentricity',
+                  'equivalent_diameter',
+                  'euler_number',
+                  'filled_area',
+                  'inertia_tensor',
+                  'inertia_tensor_eigvals',
+                  'major_axis_length',
+                  'max_intensity',
+                  'mean_intensity',
+                  'min_intensity',
+                  'minor_axis_length',
+                  'moments_hu',
+                  'moments_normalized',
+                  'perimeter',
+                  'solidity',
+                  'weighted_moments_hu',
+                  'weighted_moments_normalized']
+
+
+    # measure the largest particle
+    largest_props = measure.regionprops_table(
+        label_image=(img_labelled==particle_label).astype(np.uint8),
+        intensity_image=img,
+        properties=prop_names
+    )
+    # largest_props['objid'] = df.index[i]
+
+    largest_props = pd.DataFrame(largest_props)
+    # features = features.append(largest_props, verify_integrity=True)
+    # NB: prevent duplicate indexes
+
+    # extract the particle
+    part = regions[particle_label-1].intensity_image
+    # NB: the non particle region is marked black
+    # re-invert it
+    part = 255 - part
+    # and save it as an image
+    io.imsave(fname=dest, arr=part)
+    
+    return(largest_props)
+
+# Process several images defined in a DataFrame
+def process_images(df, cfg):
+  props = [process_image(df['source'].values[i], df['dest'].values[i], cfg) for i in range(df.shape[0])]
+  return(pd.concat(props, ignore_index=True))
