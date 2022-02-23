4636f1f767e402964a4c12b6db72e8118e14e273
Jean-Olivier Irisson <irisson@normalesup.org>
2022-02-23 14:57:55 +0000

    Remove unused objects

--- 3.regroup_taxo_and_split.R
+++ 3.regroup_taxo_and_split.R
@@ -26,6 +26,8 @@ groups = read_csv(url, col_types=cols()) %>%
 
 # add taxonomic grouping to extracted data
 df = left_join(df, groups, by=c("txo.display_name"="level0"))
+# remove objects which are not assigned at that level (ideally there should be very little to none)
+filter(df, is.na(level1))
 
 # choose grouping level
 df$taxon = df[[cfg$grouping$level]]
