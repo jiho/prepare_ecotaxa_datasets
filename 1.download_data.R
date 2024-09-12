3a0523c49979c1e27dc1e4fc48ae332018bfd877
VBancel <113167840+VBancel@users.noreply.github.com>
2024-09-12 10:29:13 +0200

    modif code based on comments

--- 1.download_data.R
+++ 1.download_data.R
@@ -1,59 +1,55 @@
 #
 #
-#Download taxa from an Ecotaxa Project
+# Download taxa from an Ecotaxa Project
+#
+# (c) 2024 Jean-Olivier Irisson, Flavien Petit and Victoria Bancel, GNU General Public License v3
 
-#install packages
-devtools::install_github("ecotaxa/ecotaxarapi")
-
-#load packages
+# Load packages
 library("ecotaxarapi")
-library(progress)
-library(yaml)
-library(dplyr)
-library(arrow)
-library(httr2)
+library("progress")
+library("yaml")
+library("dplyr")
+library("arrow")
 
-
-#read config
+# Read config
 cfg = read_yaml("config.yaml")
 print(cfg$dataset)
 
-# prepare storage -----
+# Rrepare storage -----
 data_dir <- file.path(path.expand(cfg$base_dir), cfg$dataset)
 dir.create(data_dir, showWarnings = FALSE, recursive = TRUE)
 
-
-# login to ecotaxa -----
+# Login to ecotaxa -----
 id = cfg$ecotaxa_user
-.pwd = cfg$ecotaxa_pass
+pwd = cfg$ecotaxa_pass
 
-token <- login(LoginReq=LoginReq(password=.pwd,username=id))
+token <- login(LoginReq=LoginReq(password=pwd,username=id))
 options(ecotaxa.token_path = file.path(cfg$base_dir, "token.txt"))
 save_api_token(token)
 
 
-# get validated objects and their metadata from project -----
+# Get validated objects and their metadata from project -----
 project <-  cfg$proj_id
 project_infos <- project_query(project)
 
-#fields 
+# Fields 
 objects_fields <- lapply(names(project_infos$obj_free_cols), function(k) {
   paste0('fre.', k)
 })
-#get taxonomic name and image file name and all free fields
+# Get taxonomic name and image file name and all free fields
 fields <- paste(c('txo.id,txo.display_name,img.file_name,obj.latitude,obj.longitude', objects_fields), collapse = ',')
 
-#only Validated projects
-filters <- ProjectFilters(statusfilter = "PVD") # "V" first but not all objects of Thelma
+# Only Validated projects
+filters <- ProjectFilters(statusfilter = "V")
 
-# fetch per sample
+# Fetch per sample
 samples <- samples_search(project, id_pattern = "*")
 
-# fetch one object to get the total number of objects to fetch
+# Fetch one object to get the total number of objects to fetch
 first_obj <- get_object_set(project, ProjectFilters = filters, fields = fields, window_start = 0, window_size = 1)
 print(paste0(first_obj$total_ids, " objects to fetch"))
 
-#progess bar
+# Progess bar
 pbar <- progress_bar$new(
   format = "  downloading [:bar] :percent in :elapsed",
   total = first_obj$total_ids,
@@ -61,67 +57,64 @@ pbar <- progress_bar$new(
   width = 60
 )
 
-#prepare storage
+# Prepare storage
 objs_dfs = data.frame()
 
 for (sam in unique(samples$sampleid)) {
   
-  # browser()
-  # update filters to add sampleid
+  # Update filters to add sampleid
   filters$samples <- as.character(sam)
   
-  # fetch a batch of objects
+  # Fetch a batch of objects
   objs <- get_object_set(cfg$proj_id, fields = fields, ProjectFilters = filters)
   if (length(objs$details) > 0) {
     n_fetched_objs <- nrow(objs$details)
   
-    # format retrieved data as a DataFrame + add col names from fields
-    # print(objs$details)
+    # Format retrieved data as a DataFrame + add colnames from fields
     objs_df <- as.data.frame(objs$details)
     colnames(objs_df) <- unlist(strsplit(fields, ","))
-    #add objetid
+    # Add object id
     objs_df <- mutate(objs_df, objid = objs$object_ids)
   
-   #store with the previous batches
+   # Store with the previous batches
     objs_dfs <- append(objs_dfs, list(objs_df))
   
-    # and update progress bar
+    # And update progress bar
     pbar$tick(n_fetched_objs)
   }
 }
 
-
-# combine all batches in a single DataFrame
+# Combine all batches in a single DataFrame
 df <- do.call(rbind, objs_dfs)
 
-# fix id column types
+# Fix id column types
 df$txo.id <- as.integer(df$txo.id)
 df$objid <- as.integer(df$objid)
 
-# get all unique taxa ids
+# Get all unique taxa ids
 taxo_ids <- unique(df$txo.id)
 
-# get lineage for each
+# Get lineage for each
 taxa <- lapply(taxo_ids, query_taxa)
 lineages <- sapply(taxa, function(t) {
   lineage <- rev(t$lineage)
   paste("/", paste(lineage, collapse = "/"), sep = "")
 })
 
-# add lineages to the DataFrame
+# Add lineages to the DataFrame
 taxo <- data.frame(lineages, taxo_ids) %>% rename('txo.id' = taxo_ids, 'lineage' = lineages)
 df <- df %>%  left_join(taxo, by='txo.id') 
 
-# compute number of examples per class
-inventory<-df %>% group_by(lineage, txo.id, txo.display_name) %>% summarise(n0 = n()) %>%  ungroup() %>%  rename('id' = txo.id, 'level0' = txo.display_name)
+# Compute number of examples per class
+inventory <- df %>% group_by(lineage, txo.id, txo.display_name) %>% summarise(n0 = n()) %>%  ungroup() %>%  rename('id' = txo.id, 'level0' = txo.display_name)
 
 print(paste0('Downloaded ', nrow(df), ' objects. Saving them'))
 
-#write to disk
+# Write to disk
 file_path <- file.path(data_dir, "orig_extraction.parquet")
 write_parquet(
   df,
   sink =file_path,
-  compression= NULL) #for compatiblity with R
+  compression= NULL)
 
 write.csv(inventory, file.path(data_dir, 'orig_inventory.csv'),row.names = FALSE)
--- 2.download_images.R
+++ 2.download_images.R
@@ -6,64 +6,68 @@
 library("ecotaxarapi")
 library("tools")
 library('utils')
-library(stringr)
-library(yaml)
-library(arrow)
-library(progress)
-library(dplyr)
+library("stringr")
+library("yaml")
+library("arrow")
+library("progress")
+library("dplyr")
+library("future.apply")
+library("parallel")
 
 
 
-#read config
-cfg = read_yaml("config.yaml")
-
+# Read config file
+cfg <- read_yaml("config.yaml")
 print(paste0('### Download images for ', cfg$dataset))
 
 
-# prepare storage for orig_imgs
-data_dir = file.path((cfg$base_dir), cfg$dataset)
-img_dir = file.path(data_dir, 'orig_imgs')
+# Prepare storage for orig_imgs
+data_dir <- file.path((cfg$base_dir), cfg$dataset)
+img_dir <- file.path(data_dir, 'orig_imgs')
 dir.create(img_dir, recursive = TRUE)
 
-# read data from EcoTaxa
+# Read data from EcoTaxa
 df <- read_parquet(file.path(data_dir, 'orig_extraction.parquet'))
 
-# detect image extension
-ext = file_ext(df$img.file_name)[1]
+# Detect image extension
+ext <- file_ext(df$img.file_name)[1]
 
-# name image according to internal object_id to ensure uniqueness
+# Name image according to internal object_id to ensure uniqueness
 df$dest_path = file.path(img_dir, paste0(df$objid, '.', ext))
 
-#vault_path exist ?
+# Check files existence 
+file_exists_list <- mclapply(df$dest_path, file.exists, mc.cores = detectCores()/4)
+df$file_exists <- file_exists_list
+
+# Filter missing files
+df_missing <- df %>% filter(file_exists == FALSE)
+
+# Check vault path existence
 vault_path <- '/remote/ecotaxa/vault'
 vault <- file.exists(vault_path)
 
-# download images (with a nice progress bar)
-pbar <- progress_bar$new(
-  format = "  downloading [:bar] :percent in :elapsed",
-  total = nrow(df),
-  clear = FALSE,
-  width = 60
-)
-
-for (i in 1:nrow(df)){ 
-  if (!file.exists(df$dest_path[i])){ #if the file has not been copied already
-      if (vault) {
-        res <- file.copy(
-          from = file.path(vault_path, df$path_to_img[i]),
-          to =df$dest_path[i])
-      }else {
-        res <- download.file(
-          url= paste0('https://ecotaxa.obs-vlfr.fr/vault/',df$img.file_name[i]),
-          destfile = df$dest_path[i],
-          quiet = TRUE
-        )
-      }
-    }
-  pbar$tick(1)
-
+# Function to download/copy files
+process_file <- function(row_i, dfmissing, vault) {
+  system(paste("echo 'now processing row:",row_i,"' on ", nrow(dfmissing), "row"))
+  if (vault) {
+    file.copy(
+      from = file.path(vault_path, dfmissing$img.file_name[row_i]),
+      to = dfmissing$dest_path[row_i]
+    )
+  } else {
+    download.file(
+      url = paste0('https://ecotaxa.obs-vlfr.fr/vault/', dfmissing$img.file_name[row_i]),
+      destfile = dfmissing$dest_path[row_i],
+      quiet = TRUE
+    )
+  }
 }
 
+# Apply fonction in parallel
+print("Copying or downloading images, be patient")
+mclapply(1:nrow(df_missing), process_file, dfmissing = df_missing, vault = vault, mc.cores = detectCores()/4)
+
+# Check images in directory
 n_imgs <- length(list.files(img_dir))
-print(paste0(n_imgs,' images in ', img_dir))
+print(paste0(n_imgs, ' images in ', img_dir))
 
